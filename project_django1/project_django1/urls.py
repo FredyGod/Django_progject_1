b9777d7c1a2beea559491b911b26775dffb86fa9
makle <rezer.1994@mail.ru>
2022-10-11 15:08:40 +0300

    commite_2

--- project_django1/project_django1/urls.py
+++ project_django1/project_django1/urls.py
@@ -19,7 +19,7 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('about', views.about),
+    path('about/', views.about),
     path('home/', views.home),
-    path('reverse/', views.reverse),
+    path('reverse/', views.reverse, name='reversed'),
 ]
--- project_django1/project_django1/views.py
+++ project_django1/project_django1/views.py
@@ -2,8 +2,12 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def about(request):
-    return render(request,'about.html',{'gretting':'hello'})
-def reverse(request):
-    return render(request,'reverse.html')
+    return render(request,'about.html')
+
 def home(request):
     return render(request,'home.html')
+
+def reverse(request):
+    user_text=request.GET['username']
+    reverse=user_text[::-1]
+    return render(request,'reverse.html',{'word':reverse})
